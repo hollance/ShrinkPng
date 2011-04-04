48647adbc42890f0a6f386adcc1cf6daa888e612
Matthijs Hollemans <mail@hollance.com>
2011-04-04 16:46:52 +0200

    Initial Commit

---
+++ ShrinkPng/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ShrinkPng
+//
+//  Created by Matthijs Hollemans on 04-04-11.
+//  Copyright 2011 Hollance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
